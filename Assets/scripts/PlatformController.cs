e10921dce09351f893189bf8dbb017ae96d951be
Kyle Deal <kdeal@kyledeal.com>
2016-04-20 16:52:22 -0400

    Added double jump, level ending, and respawn

--- Assets/scripts/PlatformController.cs
+++ Assets/scripts/PlatformController.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using UnityEngine.SceneManagement;
 using System.Collections;
 
 public class PlatformController : MonoBehaviour {
@@ -10,31 +11,43 @@ public class PlatformController : MonoBehaviour {
 	public float minSpeed = 10f;
 	public float maxSpeed = 100f;
 	public float deathSpeed = 2f;
-
+	public int maxJumps = 2;
 
 	private Rigidbody2D rb2d;
 	private new Collider2D collider;
+	private bool dead;
+	private int jumps;
 
 
 	// Use this for initialization
 	void Awake () {
 		rb2d = GetComponent<Rigidbody2D>();
+		rb2d.velocity = new Vector2(minSpeed, 0);
 		collider = GetComponent<Collider2D>();
+		dead = false;
 	}
 
 	// Update is called once per frame
 	void Update () {
+		if (dead) {
+			return;
+		}
+
 		bool death = Physics2D.IsTouchingLayers(collider, LayerMask.GetMask("Death"));
 
 		if (death || Mathf.Abs(rb2d.velocity.x) < 2f) {
-			Debug.Log("You Died");
+			SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+			dead = true;
 		}
 
-		Vector2 ground = transform.position;
-		ground.y -= 1f;
 		bool grounded = Physics2D.IsTouchingLayers(collider, LayerMask.GetMask("Ground"));
 
-		if (Input.GetButtonDown("Jump") && grounded) {
+		if (grounded && jumps != maxJumps) {
+			jumps = maxJumps;
+		}
+
+		if (Input.GetButtonDown("Jump") && jumps > 0) {
+			jumps--;
 			jump = true;
 		}
 
@@ -44,14 +57,18 @@ public class PlatformController : MonoBehaviour {
 	}
 
 	void FixedUpdate() {
-		float h = Input.GetAxis("Horizontal");
+		if (dead) {
+			return;
+		}
 
-		rb2d.AddForce(Vector2.right * h * moveForce);
+		float h = Input.GetAxis("Horizontal");
 		int sign = rb2d.velocity.x > 0 ? 1 : -1;
+		Vector2 v_base = sign == 1? Vector2.right: Vector2.left;
+		rb2d.AddForce(v_base * h * moveForce);
 		float vel_x = sign * Mathf.Clamp(Mathf.Abs(rb2d.velocity.x), minSpeed, maxSpeed);
 
 		if (flip) {
-			vel_x *= -1;
+			vel_x *= -1f;
 			flip = false;
 		}
 
